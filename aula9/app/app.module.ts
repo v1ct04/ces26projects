db2fc2d7f8fe51a050a32ea489d37c074d314fd5
Victor Elias <victorgelias@gmail.com>
2016-12-05 05:45:58 -0200

    Create in memory data service

--- aula9/app/app.module.ts
+++ aula9/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -13,6 +18,8 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
       {
         path: '',
