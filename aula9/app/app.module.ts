56b024e5b07b92d0fa715d00717bcbf5a82da725
Victor Gonçalves Elias <victorgelias@gmail.com>
2016-12-05 08:19:49 +0000

    Fixing systemjs error

--- aula9/app/app.module.ts
+++ aula9/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
-// Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
